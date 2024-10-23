d55aa47487c82297d9b65b9980a99b01d3a86a76
Sabuhi <sabuhi.nazarov11@gmail.com>
2024-10-23 21:29:55 +0400

    2

--- Account Service (Java)/task/src/account/UserService.java
+++ Account Service (Java)/task/src/account/UserService.java
@@ -34,61 +34,6 @@ public class UserService {
     }
 }
 
-class CustomUserDetails implements UserDetails {
-    private final User user;
-
-    public CustomUserDetails(User user) {
-        this.user = user;
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        //GrantedAuthority grantedAuthority = new SimpleGrantedAuthority("ROLE_USER");
-        //return List.of(new SimpleGrantedAuthority(grantedAuthority.getAuthority()));
-        //return List.of(new SimpleGrantedAuthority(user.getAuthority()));
-        // GrantedAuthority grantedAuthority = new SimpleGrantedAuthority("ROLE_USER");
-        // GrantedAuthority interface in Spring Security is used to represent roles and authorities
-        return List.of(new SimpleGrantedAuthority(user.getAuthority()));
-        /*@Override
-        public Collection<? extends GrantedAuthority> getAuthorities() {  if users role is more than one in entity class use collections and this here
-            return user.getAuthorities()
-                    .stream()
-                    .map(SimpleGrantedAuthority::new)
-                    .collect(Collectors.toList());
-        }*/
-    }
-
-    @Override
-    public String getPassword() {
-        return user.getPassword();
-    }
-
-    @Override
-    public String getUsername() {
-        return user.getEmail();
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
-}
-
 @Service
 class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository repository;
@@ -104,6 +49,6 @@ class CustomUserDetailsService implements UserDetailsService {
                 .findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException("Not found"));
 
-        return new CustomUserDetails(user);
+        return user;
     }
 }
--- Account Service (Java)/task/src/account/model/User.java
+++ Account Service (Java)/task/src/account/model/User.java
@@ -9,6 +9,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -16,7 +22,7 @@ import lombok.Setter;
 @Setter
 @Entity
 @Table(name = "USERS")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -33,4 +39,57 @@ public class User {
     private String password;
     @JsonIgnore
     private String authority;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        //GrantedAuthority grantedAuthority = new SimpleGrantedAuthority("ROLE_USER");
+        //return List.of(new SimpleGrantedAuthority(grantedAuthority.getAuthority()));
+        //return List.of(new SimpleGrantedAuthority(user.getAuthority()));
+
+        // GrantedAuthority grantedAuthority = new SimpleGrantedAuthority("ROLE_USER");
+        // GrantedAuthority interface in Spring Security is used to represent roles and authorities
+
+        /*@Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {  if users role is more than one in entity class use collections and this here
+            return user.getAuthorities()
+                    .stream()
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toList());
+        }*/
+
+
+        // return List.of(new SimpleGrantedAuthority(user.getAuthority()));
+        return List.of(new SimpleGrantedAuthority(authority));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
